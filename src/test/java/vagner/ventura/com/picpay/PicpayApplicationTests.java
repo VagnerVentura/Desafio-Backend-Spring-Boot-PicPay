33dd492080b15f59f404e948e36d502493f22688
vagner ventura <vagnerventura@hotmail.com>
2024-09-09 14:05:42 -0300

    Configurando conexões com docker e mysql

---
+++ src/test/java/vagner/ventura/com/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package vagner.ventura.com.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
